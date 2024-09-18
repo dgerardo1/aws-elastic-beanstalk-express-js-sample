2a3b5aad326002d0f99fe1dd4a0240b293862176
Gerardo <dgerardohernandez@gmail.com>
2024-09-18 11:43:17 -0600

    Change the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('¡Hola mundo! Desde Pipeline'));
+app.get('/', (req, res) => res.send('¡Hola mundo! Probando Pipeline desde AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
